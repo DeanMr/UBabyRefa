f582a88cef928874fda38055741fa28c65093212
UBDean <deanlzwei@gmail.com>
2016-09-01 10:44:24 +0800

    Initial Commit

---
+++ UBabyRefa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UBabyRefa
+//
+//  Created by 云宝 Dean on 16/9/1.
+//  Copyright © 2016年 云宝 Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
